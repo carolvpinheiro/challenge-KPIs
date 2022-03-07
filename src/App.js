2d5addfe4eab09e068957d564227b5a9dcbf9e8e
Carol Vasconcelos <carolinavpinheiro@gmail.com>
2022-03-07 07:43:14 -0300

    starting challenge

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>olá mundo</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+//import { render, screen } from '@testing-library/react';
+//import App from './App';
+
+//test('renders olá mundo', () => {
+//  render(<App />);
+//  const olaMundo = screen.getByText(/olá mundo/i);
+//  expect(olaMundo).toBeInTheDocument();
+//});
