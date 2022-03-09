1399c15a4058a6cfb294be3e92165d9399cbac66
Carol Vasconcelos <carolinavpinheiro@gmail.com>
2022-03-09 18:08:05 -0300

    react-router config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './styles.css'
+import Home from './pages/Home'
+import QuestionOne from './pages/QuestionOne'
+import QuestionTwo from './pages/QuestionTwo'
+import Results from './pages/Results'
 
-function App() {
-  return (
-    <div className="App">
-      <p>olá mundo</p>
-    </div>
-  );
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+//import firebaseconfig from './config/firebaseconfig'
+
+export default function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" component={ Home } />
+                <Route exact path="/question-one" component={ QuestionOne } />
+                <Route exact path="/question-two" component={ QuestionTwo } />
+                <Route exact path="/results" component={ Results } />
+            </Routes>
+        </Router>
+    );
 }
-
-export default App;
--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-//import { render, screen } from '@testing-library/react';
-//import App from './App';
-
-//test('renders olá mundo', () => {
-//  render(<App />);
-//  const olaMundo = screen.getByText(/olá mundo/i);
-//  expect(olaMundo).toBeInTheDocument();
-//});
